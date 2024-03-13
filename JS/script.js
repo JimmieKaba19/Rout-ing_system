74f5130394f04844aa1581336c125640018036ff
JimmieKaba19 <wambugujimmie994@gmail.com>
2024-03-13 08:08:21 +0300

    Rout-ing system changes being made to the interface design

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,14 @@
+//removing access to contextmenu
+addEventListener("contextmenu", (e) => {
+    //e.preventDefault();
+});
+// booking page action
+/*const booking = document.querySelector(".hero-btn");
+booking.addEventListener("click", () => {
+    window.open("./booking.html", "self");
+});*/
+
+//opening and closing contact form
 const open = document.querySelector(".open-form");
 const form = document.querySelector(".form-section");
 const bform = document.querySelector(".contact-bform");
@@ -14,3 +25,43 @@ close.addEventListener("click", () => {
     form.style.display = "none";
     bform.style.display = "block";
 });
+
+const faqTop = document.querySelector("#booking");
+const faqBottom = document.querySelector("#member");
+
+const subBooking = document.querySelector("#sub-booking");
+const subMember = document.querySelector("#sub-member");
+
+faqTop.addEventListener("click", () => {
+    if(subBooking.style.display == "block"){
+        subBooking.style.display = "none";
+    } else {
+        subBooking.style.display = "block";
+        subMember.style.display = "none";
+    }
+});
+
+faqBottom.addEventListener("click", () => {
+    if(subMember.style.display == "block"){
+        subMember.style.display = "none";
+    } else {
+        subMember.style.display = "block";
+        subBooking.style.display = "none";
+    }
+});
+
+
+// faq.addEventListener("click", (event) => {
+//     //alert("clicked");
+//     let target = event.target;
+//     switch(target.id){
+//         case "booking":
+//             alert("Clicked booking");
+//             break;
+//         case "member":
+//             alert("clicked member");
+//             break;
+//     };
+
+//     //subFaqs.style.display = "block";
+// });
